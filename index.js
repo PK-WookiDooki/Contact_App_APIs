4b13f8b2295cfe0496f2cea211dd6b2df18aae5a
Pyaesone Khant <pyaepsk@gmail.com>
2023-08-13 17:29:46 +0630

    fixing CORS

--- index.js
+++ index.js
@@ -2,20 +2,20 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const sql = require("mysql2");
 const app = express();
 const mongoose = require("mongoose");
 const connectDB = require("./config/dbConn");
+const corsOptions = require("./config/corsOptions");
 const PORT = process.env.PORT || 3500;
 
 connectDB();
 
 //middleware
+app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(bodyParser.json());
-app.use(cors());
 
 app.use("/contacts", require("./routes/contactsRoutes"));
 
